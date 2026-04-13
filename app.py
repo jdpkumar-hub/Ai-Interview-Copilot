c8c4058734ec4442c064cd7343c86f6dcef7b868
Pradarshan Jupidi <jdpkumar@gmail.com>
2026-04-13 00:19:44 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -128,10 +128,19 @@ with col2:
     rtc_configuration={
         "iceServers": [
             {"urls": ["stun:stun.l.google.com:19302"]},
+            {
+                "urls": ["turn:openrelay.metered.ca:80"],
+                "username": "openrelayproject",
+                "credential": "openrelayproject",
+            },
+            {
+                "urls": ["turn:openrelay.metered.ca:443"],
+                "username": "openrelayproject",
+                "credential": "openrelayproject",
+            },
         ]
     },
 )
-
     question_placeholder = st.empty()
     answer_placeholder = st.empty()
 
